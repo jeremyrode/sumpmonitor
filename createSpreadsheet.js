e64726b981aa98193959a55aec9e54efce7b8cdd
Jeremy Rode <jprode@gmail.com>
2024-06-07 18:13:19 -0700

    Inital Testing

--- createSpreadsheet.js
+++ createSpreadsheet.js
@@ -2,28 +2,86 @@
 
 /**
  * Create a google spreadsheet
- * Modified from Google Example, but using non-sensitive scopes and an API key rather than OAuth
+ * Modified from Google Example
  */
 
-const {google} = require('googleapis');
+ const fs = require('fs').promises;
+ const path = require('path');
+ const process = require('process');
+ const {authenticate} = require('@google-cloud/local-auth');
+ const {google} = require('googleapis');
 
-const API_KEY_PATH = '/home/pi/google_api_key.json';
+ // If modifying these scopes, delete token.json.
+ const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
+ // The file token.json stores the user's access and refresh tokens, and is
+ // created automatically when the authorization flow completes for the first
+ // time.
+ const TOKEN_PATH = '/home/pi/sump_token.json';
+ const CREDENTIALS_PATH = '/home/pi/sump_credentials.json';
 
-// Load API Key from a local json file so I don't check it into github
-fs.readFile(API_KEY_PATH, (err, content) => {
-  if (err) {
-    console.log('Error loading API key file:', err);
-  }
-  else {
-    // Authorize a client with credentials, then call the Google Calendar API.
-    const obj = JSON.parse(content);
-    console.log('Program Successfully Started');
-    create('SumpLog', obj.api_key);
-  }
-});
+ /**
+  * Reads previously authorized credentials from the save file.
+  *
+  * @return {Promise<OAuth2Client|null>}
+  */
+ async function loadSavedCredentialsIfExist() {
+   try {
+     const content = await fs.readFile(TOKEN_PATH);
+     const credentials = JSON.parse(content);
+     return google.auth.fromJSON(credentials);
+   } catch (err) {
+     return null;
+   }
+ }
 
-async function create(title, api_key) {
-  const service = google.sheets({version: 'v4', auth: api_key});
+ /**
+  * Serializes credentials to a file compatible with GoogleAuth.fromJSON.
+  *
+  * @param {OAuth2Client} client
+  * @return {Promise<void>}
+  */
+ async function saveCredentials(client) {
+   const content = await fs.readFile(CREDENTIALS_PATH);
+   const keys = JSON.parse(content);
+   const key = keys.installed || keys.web;
+   const payload = JSON.stringify({
+     type: 'authorized_user',
+     client_id: key.client_id,
+     client_secret: key.client_secret,
+     refresh_token: client.credentials.refresh_token,
+   });
+   await fs.writeFile(TOKEN_PATH, payload);
+ }
+
+ /**
+  * Load or request or authorization to call APIs.
+  *
+  */
+ async function authorize() {
+   let client = await loadSavedCredentialsIfExist();
+   if (client) {
+     return client;
+   }
+   client = await authenticate({
+     scopes: SCOPES,
+     keyfilePath: CREDENTIALS_PATH,
+   });
+   if (client.credentials) {
+     await saveCredentials(client);
+   }
+   return client;
+ }
+
+
+
+ /**
+ * Create a google spreadsheet
+ * @param {string} title Spreadsheets title
+ * @return {string} Created spreadsheets ID
+ */
+async function create(auth) {
+  const title = 'sump_test_jer';
+  const service = google.sheets({version: 'v4', auth});
   const resource = {
     properties: {
       title,
@@ -37,8 +95,10 @@ async function create(title, api_key) {
     console.log(`Spreadsheet ID: ${spreadsheet.data.spreadsheetId}`);
     return spreadsheet.data.spreadsheetId;
   } catch (err) {
-    console.log('Error creating spreadsheet');
+    // TODO (developer) - Handle exception
     throw err;
   }
 }
 
+ authorize().then(create).catch(console.error);
+ // [END calendar_quickstart]
