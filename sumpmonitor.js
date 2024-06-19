8436158c5927ad09fad97be44d9213f9f9ac1c10
Jeremy Rode <jprode@gmail.com>
2024-06-18 17:33:51 -0700

    Current Meter Working

--- sumpmonitor.js
+++ sumpmonitor.js
@@ -13,10 +13,12 @@ var ip = require("ip");
 const DATA_LOG_FILE = '/home/jprode/SumpData.csv';
 const ERR_LOG_FILE = '/home/jprode/SumpErrorLog.txt';
 const MEASUREMENT_INTERVAL = 30;
-const WRITE_TO_GOOGLE_INTERVAL = 100;  //How many datapoints to capture between Google API Calls
-const DATA_CAPTURE_INTERVAL = 10; //How many ADC samples averaged into a datapoint
-const ZERO_LEVEL_CODE = 3084.327283; //Might be altitude dependent
-const DEPTH_SLOPE = 148.93; //Codes per inch
+const WRITE_TO_GOOGLE_INTERVAL = 2;  //How many datapoints to capture between Google API Calls
+const DATA_CAPTURE_INTERVAL = 100; //How many ADC samples averaged into a datapoint (10 ~= 3s)
+const ZERO_LEVEL_CODE = 3084.327283; //Code at Zero water level, Might be altitude/temp dependent
+const DEPTH_SLOPE = 148.93; //Codes per inch, prob temp dependent
+const ZERO_CURRENT_CODE = 0;
+const CURRENT_SLOPE = 1;
 
 const dayFraction = 86400000; // Milliseconds in a Day
 const dateOffset = new Date(1899,11,30) - 3600000;  // Spreadsheet Epoc minus an hour
@@ -30,13 +32,21 @@ const GoogleAuth = new google.auth.GoogleAuth({
 let auth = [];
 auth.expiryDate = 0;
 
+let measurmentArray = [];
+
 lcd.beginSync();
 lcd.clearSync();
-lcd.printLineSync(3, ip.address());
+printIPAddress();
+setInterval(printIPAddress, 1000000); //Update IP
+setInterval(AppendSpreadSheet, 100000); //Send data to Google
 
-async function AppendSpreadSheet(measurmentArray) {
+function printIPAddress() {
+  lcd.printLineSync(3, ip.address());
+}
+
+async function AppendSpreadSheet() {
    if (auth.expiryDate < Date.now()) {
-    logWithTime('Credentials Expired, getting new ones');
+    logWithTime('Getting New Google Credentials');
     auth = await GoogleAuth.getClient();
   }
   sheets.spreadsheets.values.append({
@@ -53,41 +63,48 @@ async function AppendSpreadSheet(measurmentArray) {
         logWithTime('Append Threw: ' + err);
     } else if (!result.statusText === 'OK') {
         logWithTime('Google Says Not OK: ' + result);
+    } else {
+      measurmentArray = []; //If sucess clear out stored measurments
     }
   });
 }
 
 ADS1115.open(0, 0x48).then(async (ads1115) => {
-  let measurmentArray = [];
   let mesaurementCount = 0;
   ads1115.gain = 1;
+  let strcurDate_old = ' ';
   let ave_level = await ads1115.measure('0+GND');
-  let  ave_current = await ads1115.measure('1+GND');
+  let  ave_current = 0;
   logWithTime('New Session');
   while (true) {
-    let cur_level = await ads1115.measure('0+GND');  // DATA_CAPTURE_INTERVAL * ();
-    let cur_current = await ads1115.measure('1+GND');
+    let cur_level = await ads1115.measure('0+3'); //This never gets near zero
+    let cur_current = await ads1115.measure('2+3'); //This can go slightly below zero
+    if (cur_current > 32768) { // 2's compliment
+      cur_current = (cur_current - 65536);
+    }
     ave_level = cur_level / DATA_CAPTURE_INTERVAL + ave_level * (DATA_CAPTURE_INTERVAL - 1) / DATA_CAPTURE_INTERVAL;
     let depth_inches = (ave_level - ZERO_LEVEL_CODE) / DEPTH_SLOPE;
     ave_current = cur_current / DATA_CAPTURE_INTERVAL + ave_current * (DATA_CAPTURE_INTERVAL - 1) / DATA_CAPTURE_INTERVAL;
+    let current_amps = (ave_current - ZERO_CURRENT_CODE) / CURRENT_SLOPE;
     const curDate = new Date();
-    const strcurData = curDate.toString();
-    if (mesaurementCount >= DATA_CAPTURE_INTERVAL) {
-      measurmentArray.push([(curDate - dateOffset) / dayFraction, depth_inches, ave_current]);
-      datafile.write(curDate.valueOf() + ',' + depth_inches + ',' + ave_current + '\n');
+    const strcurDate = curDate.toString();
+    if (mesaurementCount >= DATA_CAPTURE_INTERVAL) { //Take a datapoint 
+      measurmentArray.push([(curDate - dateOffset) / dayFraction, depth_inches.toFixed(3), ave_current.toFixed(3)]);
+      datafile.write(curDate.valueOf() + ',' + depth_inches.toFixed(3) + ',' + ave_current.toFixed(3) + '\n');
       mesaurementCount = 0;
-      if (measurmentArray.length >= WRITE_TO_GOOGLE_INTERVAL) {
-          AppendSpreadSheet(measurmentArray);
-          measurmentArray = [];
-      }   
     }
-    lcd.printLineSync(0, strcurData.slice(0,15));
-    lcd.printLineSync(1, strcurData.slice(16,24));
-    lcd.printLineSync(2, depth_inches.toFixed(2).toString().padStart(6));
+    if ( strcurDate.slice(0,15) != strcurDate_old.slice(0,15) ) { //Only print if it changes
+      lcd.printLineSync(0, strcurDate.slice(0,15));
+    }
+    if ( strcurDate.slice(16,24) != strcurDate_old.slice(16,24) ) { //Only print if it changes
+      lcd.printLineSync(1, strcurDate.slice(16,24));
+    }
+    //lcd.printLineSync(2, depth_inches.toFixed(2).toString().padStart(6) + ' ' + cur_current.toString());
+    lcd.printLineSync(2, depth_inches.toFixed(2).toString().padStart(6) + cur_current.toString().padStart(6));
     mesaurementCount += 1;
+    strcurDate_old = strcurDate;
   }
 })
-
 
 //log function
 function logWithTime(errmsg) {
