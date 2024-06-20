18fd76f6b86e3e6a352ba1973baeb29ad10d5743
Jeremy Rode <jprode@gmail.com>
2024-06-20 11:07:18 -0700

    current working

--- sumpmonitor.js
+++ sumpmonitor.js
@@ -17,8 +17,6 @@ const WRITE_TO_GOOGLE_INTERVAL = 2;  //How many datapoints to capture between Go
 const DATA_CAPTURE_INTERVAL = 100; //How many ADC samples averaged into a datapoint (10 ~= 3s)
 const ZERO_LEVEL_CODE = 3084.327283; //Code at Zero water level, Might be altitude/temp dependent
 const DEPTH_SLOPE = 148.93; //Codes per inch, prob temp dependent
-const ZERO_CURRENT_CODE = 0;
-const CURRENT_SLOPE = 1;
 
 const dayFraction = 86400000; // Milliseconds in a Day
 const dateOffset = new Date(1899,11,30) - 3600000;  // Spreadsheet Epoc minus an hour
@@ -74,23 +72,29 @@ ADS1115.open(0, 0x48).then(async (ads1115) => {
   ads1115.gain = 1;
   let strcurDate_old = ' ';
   let ave_level = await ads1115.measure('0+GND');
-  let  ave_current = 0;
+  let ave_current = 0;
+  let current_amps = 0;
   logWithTime('New Session');
   while (true) {
     let cur_level = await ads1115.measure('0+3'); //This never gets near zero
     let cur_current = await ads1115.measure('2+3'); //This can go slightly below zero
-    if (cur_current > 32768) { // 2's compliment
+    if (cur_current > 32768) { // 2's compliment crosses near zero sometimes
       cur_current = (cur_current - 65536);
     }
     ave_level = cur_level / DATA_CAPTURE_INTERVAL + ave_level * (DATA_CAPTURE_INTERVAL - 1) / DATA_CAPTURE_INTERVAL;
-    let depth_inches = (ave_level - ZERO_LEVEL_CODE) / DEPTH_SLOPE;
+    let depth_inches = (ave_level - ZERO_LEVEL_CODE) / DEPTH_SLOPE; //Seems to be linear
     ave_current = cur_current / DATA_CAPTURE_INTERVAL + ave_current * (DATA_CAPTURE_INTERVAL - 1) / DATA_CAPTURE_INTERVAL;
-    let current_amps = (ave_current - ZERO_CURRENT_CODE) / CURRENT_SLOPE;
+    if (ave_current > 180) {
+      current_amps = 0.00391216*(ave_current - 0.4) + 5.15725; // Linear at high current
+    } else {
+      current_amps = 6.14e-2*(ave_current - 0.4) - 1.6e-4*ave_current*ave_current; // Not linear at low current
+    }
     const curDate = new Date();
     const strcurDate = curDate.toString();
     if (mesaurementCount >= DATA_CAPTURE_INTERVAL) { //Take a datapoint 
-      measurmentArray.push([(curDate - dateOffset) / dayFraction, depth_inches.toFixed(3), ave_current.toFixed(3)]);
-      datafile.write(curDate.valueOf() + ',' + depth_inches.toFixed(3) + ',' + ave_current.toFixed(3) + '\n');
+      console.log('Current: ' + ave_current.toFixed(3)) //Testing zero point for current
+      measurmentArray.push([(curDate - dateOffset) / dayFraction, depth_inches.toFixed(3), current_amps.toFixed(3)]);
+      datafile.write(curDate.valueOf() + ',' + depth_inches.toFixed(3) + ',' + current_amps.toFixed(3) + '\n');
       mesaurementCount = 0;
     }
     if ( strcurDate.slice(0,15) != strcurDate_old.slice(0,15) ) { //Only print if it changes
@@ -99,8 +103,7 @@ ADS1115.open(0, 0x48).then(async (ads1115) => {
     if ( strcurDate.slice(16,24) != strcurDate_old.slice(16,24) ) { //Only print if it changes
       lcd.printLineSync(1, strcurDate.slice(16,24));
     }
-    //lcd.printLineSync(2, depth_inches.toFixed(2).toString().padStart(6) + ' ' + cur_current.toString());
-    lcd.printLineSync(2, depth_inches.toFixed(2).toString().padStart(6) + cur_current.toString().padStart(6));
+    lcd.printLineSync(2, depth_inches.toFixed(2).toString().padStart(6) + current_amps.toFixed(2).toString().padStart(6));
     mesaurementCount += 1;
     strcurDate_old = strcurDate;
   }
